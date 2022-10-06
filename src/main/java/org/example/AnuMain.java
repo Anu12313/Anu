b8e4395471eb4765b4ffd1cefa230d89455d0528
Anu12313 <anu100168a@gmail.com>
2022-10-06 06:10:02 +0530

    intial commit

---
+++ src/main/java/org/example/AnuMain.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class AnuMain {
+    public static void main(String[] args) {
+
+        System.out.println("AnuMain class");
+    }
+}
---
+++ src/test/java/AnuTest.java
@@ -0,0 +1,6 @@
+public class AnuTest {
+    public static void main(String[] args) {
+
+        System.out.println("AnuTest class");
+    }
+}
